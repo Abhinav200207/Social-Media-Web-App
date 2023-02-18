8a7ab982a4dc2dcd7ec45d6f1c2bcb8749b40e49
Abhinav Kumar Maurya <82315896+Abhinav200207@users.noreply.github.com>
2023-02-18 16:24:43 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 exports.connectDatabase = () => {
     console.log("Yahan tak aareha hai bhai");
-    return mongoose.connect("mongodb+srv://Abhinav:3290238@nodejsproject.pwxex.mongodb.net/?retryWrites=true&w=majority").then((data) => {
+    return mongoose.connect("").then((data) => {
         console.log(`Connected to MongoDB database ${data}`);
     }).catch((err) => {
         console.log(`ERROR!!!! connecting to MongoDB database ${err.message}`);
